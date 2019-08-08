a117c7d9ee876009b76bfdde9eb7ebfcfa08c515
232171773@qq.com <667976dsa>
2019-08-08 12:56:39 +0800

    新建 Start Scene 和 Splash Scene 并跳转场景

---
+++ Assets/Scipts/levelLoadScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoadScript : MonoBehaviour {
+    int currentSceneIndex;
+
+    void Start() {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(currentSceneIndex);
+        if (currentSceneIndex == 0) {
+            StartCoroutine(WaitForTimeToLoadNextScene(4));
+        }
+    }
+
+    IEnumerator WaitForTimeToLoadNextScene(float timeToWait) {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene() {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
---
+++ learn.md
@@ -0,0 +1,1 @@
+* 通过 Main Camera 的 Background 设置黑边背景色
