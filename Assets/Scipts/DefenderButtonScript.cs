9415ed7fec4e9925bb0aaa9bc228e12cae4a95ca
232171773@qq.com <667976dsa>
2019-08-15 16:29:50 +0800

    添加 buttons

---
+++ Assets/Scipts/DefenderButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButtonScript : MonoBehaviour
+{
+    void OnMouseDown() {
+        var buttons = FindObjectsOfType<DefenderButtonScript>();
+        foreach (DefenderButtonScript btn in buttons) {
+            if (btn != this) {
+                btn.GetComponent<SpriteRenderer>().color = new Color32(41,41,41,255);
+            }
+            else {
+                GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+    }
+}
