296ce085f2012ebd7db97b145a392004d0c0801a
232171773@qq.com <667976dsa>
2019-08-16 16:13:55 +0800

    完成 lazard attacker 伤害逻辑

--- Assets/Scipts/AttackerScript.cs
+++ Assets/Scipts/AttackerScript.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class AttackerScript : MonoBehaviour {
     [Range(0.2f, 5f)] [SerializeField] float walkSpeed = 1f;
+    [SerializeField] int damage = 10;
     float currentSpeed = 0;
     GameObject currentTarget;
 
     void Update() {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
     }
 
+    void UpdateAnimationState() {
+        if (!currentTarget) {
+            GetComponent<Animator>().SetBool("isAttacking",false);
+        }
+    }
     public void SetMovementSpeed(float speed) {
         currentSpeed = speed * walkSpeed;
     }
@@ -18,5 +25,16 @@ public class AttackerScript : MonoBehaviour {
     public void Attack(GameObject target) {
         GetComponent<Animator>().SetBool("isAttacking",true);
         currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget() {
+        if (!currentTarget) {
+            return;
+        }
+
+        HealthScript health = currentTarget.GetComponent<HealthScript>();
+        if (health) {
+            health.DealDamage(damage);
+        }
     }
 }
