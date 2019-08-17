e610a4cb295b207682fcf484162472b1637402c3
232171773@qq.com <667976dsa>
2019-08-17 16:39:55 +0800

    添加 fox attacker

---
+++ Assets/Scipts/FoxScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoxScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D otherCollider) {
+        GameObject otherObject = otherCollider.gameObject;
+
+        if (otherObject.GetComponent<GravestoneScript>()) {
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }else if (otherObject.GetComponent<DefenderScript>()) {
+            GetComponent<AttackerScript>().Attack(otherObject);
+        }
+    }
+}
---
+++ Assets/Scipts/GravestoneScript.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravestoneScript : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D otherCollider) {
+        AttackerScript attacker = otherCollider.GetComponent<AttackerScript>();
+        if (attacker) {
+            // attacker.Attack(gameObject);
+        }
+    }
+}
